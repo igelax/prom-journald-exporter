fc4112c3f80de90b49d411033c94bc88323881da
Mike S <mikeysky@gmail.com>
2022-01-02 20:48:02 -0700

    test journald reading and prom metric counting

--- main_test.go
+++ main_test.go
@@ -1,19 +1,31 @@
 package main
 
 import (
-	_ "bufio"
-	_ "context"
+	"bufio"
+	"context"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	_ "strings"
-	"sync"
+	"os/exec"
+	"regexp"
+	"strings"
 	"testing"
 	"time"
 )
 
 func testRoutine(t *testing.T) {
+
+	time.Sleep(time.Second * 2)
+
+	cmd := exec.Command("logger", "sudo:session")
+
+	err := cmd.Run()
+
+	if err != nil {
+		t.Error("Expected logger shell command to generate a log message, got", err)
+	}
+
 	time.Sleep(time.Second * 2)
 
 	resp, err := http.Get("http://localhost:9101/metrics")
@@ -30,33 +42,44 @@ func testRoutine(t *testing.T) {
 	// Convert the body to type string
 	sb := string(body)
 
-	// scanner := bufio.NewScanner(strings.NewReader(sb))
-	// for scanner.Scan() {
-	// 	fmt.Println(scanner.Text())
-	// }
-
-	fmt.Print(sb)
-	panic(0)
+	// Scan results for regexp match indicating test entry was counted by Prometheus
+	logMatch := false
+	r, _ := regexp.Compile("^sudo_count_total 1$")
+	scanner := bufio.NewScanner(strings.NewReader(sb))
+	for scanner.Scan() {
+		matched := r.MatchString(scanner.Text())
+		if matched {
+			logMatch = true
+		}
+		//fmt.Println(scanner.Text())
+	}
+	if !logMatch {
+		t.Error("Expected to find test journald log message counted in Prometheus metrics")
+	}
 
 }
 
 func TestProm(t *testing.T) {
 
 	var (
-		// ctx, cancel = context.WithCancel(context.Background())
-		listenHTTP string
+		listenHTTP  string
+		debug       bool
+		ctx, cancel = context.WithCancel(context.Background())
 	)
+
+	defer func() {
+		// Shutdown. Cancel application context will kill all attached tasks.
+		fmt.Println("Running cancel()")
+		cancel()
+	}()
 
 	// command line (flag) variables and defaults
 	flag.StringVar(&listenHTTP, "listenHTTP", ":9101", "ip:port to listen for http requests")
 	flag.BoolVar(&debug, "debug", false, "Enable debug")
 	flag.Parse()
 
-	var wg sync.WaitGroup
-	wg.Add(3)
-	go read_journal()        // go routine to follow/tail new journald logs and process metrics
-	go prom_http(listenHTTP) // start Prometheus http endpoint
-	go testRoutine(t)
-	wg.Wait()
+	go read_journal(ctx, "")      // go routine to follow/tail new journald logs and process metrics
+	go prom_http(ctx, listenHTTP) // start Prometheus http endpoint
+	testRoutine(t)
 
 }
